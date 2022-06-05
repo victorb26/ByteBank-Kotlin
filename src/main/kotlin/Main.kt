767cab5bf68aeef8827f6594b31da9592e8b39bd
Victor Barros <victorjuda49@gmail.com>
2022-06-05 17:30:43 -0300

    primeiro commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Welcome to ByteBank!")
+    val titular = "Victor"
+    val numeroConta = 100
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo +=  200
+
+    println("titular $titular")
+    println("numero da conta $numeroConta")
+    println("saldo $saldo")
+}
